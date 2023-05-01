414d17a2558c4be0d0dd852a7bffa964ddb1706c
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-01 10:47:48 +0600

    recap with login route same as register route

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import app from '../../firebase.config';
+
+const auth = getAuth(app);
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+            const userLogged = result.user;
+            setSuccess('User Login successful.')
+            setError('')
+        })
+        .catch(error =>{
+            setError(error.message)
+        })
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='w-25 mx-auto'>
+            <h2>Please Login</h2>
+            <form onSubmit={handleLogin} >
+                <div className="form-group mb-3">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" name='email'  className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required />
+
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' className="form-control" id="password" placeholder="Password" required />
+                </div>
+                <div className="form-check mb-3">
+                    <input type="checkbox" className="form-check-input" id="rememberMe" />
+                    <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            <p><small>Forget Password? Please <button  className='btn btn-link'>Reset Password</button></small></p>
+            <p><small>New to this website? Please <Link to="/register">Register</Link></small></p>
+            
         </div>
     );
 };
